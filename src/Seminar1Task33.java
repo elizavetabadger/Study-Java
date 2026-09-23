078a1cec8ebe6415a03a1466ac7018d55ca7ad93
Lizabel <Artbel543@yandex.ru>
2026-09-23 12:42:25 +0300

    Seminar1Task33

--- src/Seminar1Task33.java
+++ src/Seminar1Task33.java
@@ -15,4 +15,18 @@
  */
 
 public class Seminar1Task33 {
+    static void twoBandits(){
+        // https://acmp.ru/index.asp?main=task&id_task=33
+
+        int shotByHarry = Integer.parseInt(IO.readln("Сколько банок прострелил Гарри: "));
+        int shotByLarry = Integer.parseInt(IO.readln("Сколько банок прострелил Ларри: "));
+
+        int totalPots = shotByHarry + shotByLarry -1;
+        System.out.println("total pots = "+totalPots);
+
+        IO.println("Гарри не прострелил "+ (totalPots-shotByHarry));
+        IO.println("Ларри не прострелил "+ (totalPots-shotByLarry));
+
+        IO.println((totalPots - shotByHarry)+ " " +(totalPots - shotByLarry));
+    }
 }
