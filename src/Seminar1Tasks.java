4b2710c302a867a3a711215697ea1319b7c96d7f
Lizabel <Artbel543@yandex.ru>
2026-09-24 22:52:12 +0300

    Tasks

--- src/Seminar1Tasks.java
+++ src/Seminar1Tasks.java
@@ -1,18 +1,96 @@
+import java.util.Scanner;
 
 public class Seminar1Tasks {
-    static void twoBandits(){
+
+    public static void main() {
+//        twoBandits();
+//        busExcursions2();
+//        paperCrane();
+        solitaireShapoklyak();
+    }
+
+    private static void twoBandits() {
         // https://acmp.ru/index.asp?main=task&id_task=33
 
         int shotByHarry = Integer.parseInt(IO.readln("Сколько банок прострелил Гарри: "));
         int shotByLarry = Integer.parseInt(IO.readln("Сколько банок прострелил Ларри: "));
 
-        int totalPots = shotByHarry + shotByLarry -1;
-        System.out.println("total pots = "+totalPots);
+        int totalPots = shotByHarry + shotByLarry - 1;
+        System.out.println("total pots = " + totalPots);
 
-        IO.println("Гарри не прострелил "+ (totalPots-shotByHarry));
-        IO.println("Ларри не прострелил "+ (totalPots-shotByLarry));
+        IO.println("Гарри не прострелил " + (totalPots - shotByHarry));
+        IO.println("Ларри не прострелил " + (totalPots - shotByLarry));
 
-        IO.println((totalPots - shotByHarry)+ " " +(totalPots - shotByLarry));
+        IO.println((totalPots - shotByHarry) + " " + (totalPots - shotByLarry));
     }
-    // 72 Журавлики
+
+    private static void paperCrane() {
+        // https://acmp.ru/index.asp?main=task&id_task=92
+        System.out.println("Введите натуральное число от 6: ");
+        Scanner scr = new Scanner(System.in);
+        int s = scr.nextInt();
+
+        int cranePeter = s / 6;
+        int craneKatya = cranePeter * 4;
+        int craneSerechga = cranePeter;
+
+        if (s < 6) {
+            System.out.println("Введено неправильное число :(");
+        } else
+            System.out.println(cranePeter+" " + craneKatya +" " + craneSerechga);
+
+    }
+
+    private static void busExcursions2() {
+        // https://acmp.ru/index.asp?main=task&id_task=233
+
+        System.out.println("Введите количество мостов: ");
+        Scanner sc = new Scanner(System.in);
+        int numBridges = sc.nextInt();
+        boolean crashed = false;
+
+        for (int i = 0; i < numBridges && !crashed; i++) {
+            System.out.println("Введите высоту моста № " + (i + 1));
+            int height = sc.nextInt();
+            if (height <= 437) {
+                System.out.println("Crash на мосту № " + (i + 1));
+                crashed = true;
+            }
+        }
+        if (!crashed) {
+            System.out.println("No crash - всё проехали!");
+        }
+
+    }
+
+    private  static void solitaireShapoklyak(){
+        // https://acmp.ru/index.asp?main=task&id_task=521
+        System.out.println("Введите количество карт в первой колоде: ");
+        Scanner sc = new Scanner(System.in);
+        int cardsMin = sc.nextInt();
+        System.out.println("Введите количество карт в последней колоде: ");
+        int cardsMax = sc.nextInt();
+        int amount = 0;
+
+        for (int i = cardsMin; i <= cardsMax; i++){
+
+            while (j % 2 == 0){
+                amount = j + 1;
+
+                int cards = i/2;
+
+                amount = i + 1;
+
+            }
+            if(cardsMax % 2 != 0){
+               int cards = i*3+1;
+            }
+
+            else
+                System.out.println("Пасьянс не сошёлся");
+
+        }
+        System.out.println("Шапокляк брала карты "+ amount + " раз");
+
+    }
 }
