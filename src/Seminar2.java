244b2e5b54b007413a5501b4dc86f81aecd7d679
Lizabel <Artbel543@yandex.ru>
2026-09-25 13:04:53 +0300

    flowers

--- src/Seminar2.java
+++ src/Seminar2.java
@@ -8,7 +8,8 @@ public class Seminar2 {
 //        var4();
 //        varConst();
 //        var3();
-        exercise2();
+//        exercise2();
+        flowersVar1();
     }
 
     private  static void exercise1(){
@@ -116,26 +117,49 @@ public class Seminar2 {
     }
 
     private  static void exercise2(){
-        final  int BIG_PRICE_LEVEL = 300;
-        final  int MEDIUM_PRICE_LEVEL = 150;
-        final  int SMALL_PRICE_LEVEL = 80;
-
-        System.out.println("Сколько стоит чашка кофе? ");
-        String message = "Наше мнение о кофе ";
+//        Много раз Попросить пользователя вводить целые числа
+//        Остановиться, когда сумма введенных чисел достигнет 42
+//
+//        Рассказать, сколько всего он ввел чисел
+//        + Каков процент количества положительных чисел?
+//        + Чему равна сумма отрицательных чисел?
 
         Scanner sc = new Scanner(System.in);
-        int priceOfCoffe = sc.nextInt();
+        int sum = 0;
+        int count = 0;
+        int countPos = 0;
+        int sumNeg = 0;
 
-        if (priceOfCoffe > BIG_PRICE_LEVEL ){
-            message ="Дороговато";
-        } else if (priceOfCoffe >= MEDIUM_PRICE_LEVEL ){
-            message ="Норм";
-        }  else if (priceOfCoffe >= SMALL_PRICE_LEVEL ){
-            message ="Дёшево";
-        }  else {
-            message = "А это в рублях? А это вообще кофе?";
+        do {
+            int x = Integer.parseInt(IO.readln("Введите очередное целое число: "));
+            sum += x;
+            count++;
+            if (x>0) countPos ++;
+            if (x <0) sumNeg += x;
         }
-        System.out.println(message);
+        while (sum < 42);
+        IO.println("Всего было введено "+ count +" чисел");
+        IO.println("Процент положительных чисел равен "+ (100.0*countPos/count)+" %");
+        IO.println("Сумма отрицательных чисел равна "+ sumNeg);
+
     }
 
+    private static void flowersVar1(){
+        // https://acmp.ru/index.asp?main=task&id_task=504
+        int k = Integer.parseInt(IO.readln("Сколько дней? "));
+//        Начальное состояние GCV
+        String left = "G", center = "C", right = "V";
+//                Повторить k раз:
+        for (int i = 0; i < k; i++){
+            // Маша делает перестанову
+            String taburet = right;
+            right = center;
+            center = taburet;
+            // Таня делает перестановку
+            taburet = left;
+            left = center;
+            center = taburet;
+        }
+        IO.println(left + center +right);
+    }
 }
